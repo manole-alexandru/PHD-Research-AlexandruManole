86ebd0a25a09a3493639c94dbbbbb95c7fecff03
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2025-11-07 11:08:51 +0200

    Longer exp

--- components/mt-sd/experiment.py
+++ components/mt-sd/experiment.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
         DATASETS = ["cifar10", "mnist"]
 
     # Centralized constants (change once here)
-    EPOCHS = 5
+    EPOCHS = 30
     WITH_DSD = False
     TIMESTEPS = 200
     N_SAMPLE = 64
